8cfb406784f6810dc677f4b865b4b7a646d2b7b6
FENY2022 <112142042+FENY2022@users.noreply.github.com>
2026-09-23 08:30:34 +0800

    Protect AMSOS user division lookup

--- get_stations_2.php
+++ get_stations_2.php
@@ -1,5 +1,19 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once "connect.php";
+require_once "role_access.php";
+
+if (
+    !isset($_SESSION['loggedin']) ||
+    $_SESSION['loggedin'] !== ($_SESSION['usernameSRF'] ?? null) ||
+    !amsos_can_manage_configuration($_SESSION['User_RoleSRF'] ?? '')
+) {
+    http_response_code(403);
+    exit;
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['office'])) {
     $selectedOffice = $_POST['office'];
